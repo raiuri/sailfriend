ccad18197f8e6cec356b1188650a36cdb4593649
Raiuri Santos <laxlax@Raiuris-MacBook-Pro.local>
2024-09-30 14:15:07 -0300

    configured postgres database to run with docker

--- config/settings.py
+++ config/settings.py
@@ -98,15 +98,19 @@ REST_AUTH = {
 
 # Allouth configs
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
