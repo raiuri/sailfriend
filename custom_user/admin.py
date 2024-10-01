08a09f2bd685aaab1004992f4b105a7627d0a21b
Raiuri Santos <laxlax@Raiuris-MacBook-Pro.local>
2024-10-01 17:19:47 -0300

    added User to superadmin panel

--- custom_user/admin.py
+++ custom_user/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import InterestCategory
+from .models import InterestCategory, CustomUser
 
 @admin.register(InterestCategory)
 class InterestCategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     search_fields = ('name', 'id')
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_active',
+        'is_superuser',
+    )
+    search_fields = ('username', 'id')
